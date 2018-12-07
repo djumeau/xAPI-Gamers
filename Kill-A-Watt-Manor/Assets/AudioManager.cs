e01406cfd019ac7c9631a5557b547bc3fa937528
Jonathan Rock <jonathanerock@gmail.com>
2018-12-06 20:59:06 -0500

    Lights and Sounds
    Colorful BG's, Room Music, Dad Pig and Mom Pig

---
+++ Kill-A-Watt-Manor/Assets/AudioManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    public AudioSource BGM;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeBGM(AudioClip music)
+    {
+        if (BGM.clip.name == music.name)
+            return;
+
+        BGM.Stop();
+        BGM.clip = music;
+        BGM.Play();
+    }
+}
