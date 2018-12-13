97b63dcce87480f58c9716012040fb2d5e9db52f
Jonathan Rock <jonathanerock@gmail.com>
2018-12-12 20:12:42 -0500

    Added Script to make the Family Members always Face Player
    FamilyFlipper.cs got added to the Pig Destroy Effect Prefabs and it checks for Player Transform X position vs Family Members X position and changes it when player switches sides.

---
+++ Kill-A-Watt-Manor/Assets/1_ThisGame/Scripts/FamilyFlipper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FamilyFlipper : MonoBehaviour {
+
+    public GameObject deathEffect;
+    private Transform playerLoc;
+    private bool m_FacingLeft = true;  // For determining which way the family member is currently facing.
+
+    // Use this for initialization
+    void Start () {
+        playerLoc = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(playerLoc.position.x > deathEffect.transform.position.x && m_FacingLeft)
+        {
+            Flip();
+        }else if(playerLoc.position.x < deathEffect.transform.position.x && !m_FacingLeft)
+        {
+            Flip();
+        }
+	}
+
+    private void Flip()
+    {
+        // Switch the way the family member is labelled as facing.
+        m_FacingLeft = !m_FacingLeft;
+
+        transform.Rotate(0f, 180f, 0f);
+    }
+
+}
